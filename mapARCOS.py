b0c0caea00bccc825ac48c5216ca1c739eea597a
Benjamin Ryan <ben.ryan@gmail.com>
2020-10-22 08:29:31 -0400

    Fixed simple map sampling script to work with new code scheme.

--- mapARCOS.py
+++ mapARCOS.py
@@ -1,43 +1,92 @@
 
+from ARCOS.process_arcdata import aggregate_zips
 
+import argparse
 import geopandas as gpd
-import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import re
 
 from fiona.crs import from_epsg
 
-DIR = 'C:/Users/ben_ryan/Documents/Maps/zipcodes'
+
+def get_paths(path):
+    if os.path.isabs(path):
+        DIR = os.path.dirname(path)
+    else:
+        DIR = os.getcwd()
+        path = os.path.join(DIR, path)
+
+    return DIR, path
+
+def list_drugs(args):
+    datDIR, arc_path = get_paths(args.data):
+    arcDF = pd.read_csv(arc_path)
+
+    drugs = arcDF[['drug_code', 'drug_name']].copy()
+    drugs = drugs.sort_values("drug_name").drop_duplicates().reset_index(drop=True)
+
+    print(drugs.reset_index(drop=True))
+
+    return None
 
 
-# Read Spatial Data and aggregate
-zipfp = "{}/{}".format(DIR,"cb_2017_us_zcta510_500k.shp")
-zipshp = gpd.read_file(zipfp)
-zipshp['aggZip'] = zipshp.apply(lambda x: int(x['GEOID10'][0:3]), axis=1)
-zipshp = zipshp.dissolve(by='aggZip',aggfunc='sum')
-zipshp.to_file("{}/{}".format(DIR,"aggzips.shp"))
+def run(args)
+    geoDIR, zip_path = get_paths(args.geo):
+    datDIR, arc_path = get_paths(args.data):
+
+    # Read Spatial Data and aggregate
+    zipshp = gpd.read_file(zip_path)
+
+    if "aggZip" not in zipshp.columns:
+        zipshp = aggregate_zips(zipshp, zip_path)
+
+    # Read ARCOS data and population estimates
+    arcDF = pd.read_csv(arc_path)
+
+    # zipPop = pd.read_csv("{}/{}".format(datDIR,"aggzipPop.csv"))
+
+    if args.drug:
+        arcDF = arcDF[arcDF.drug_code == args.drug]
+    else:
+        args.drug = 'all'
+
+    if args.year:
+        arcDF = arcDF[pd.to_datetime(arcDF.end).apply(lambda x: x.year)==args.year]
+    else:
+        args.year = 'all'
 
 
-# Read ARCOS data and population estimates
-arcfp = "{}/{}".format(DIR,"ARCOS_2000_2016.csv")
-arcDF = pd.read_csv(arcfp)
-zipPop = pd.read_csv("{}/{}".format(DIR,"aggzipPop.csv"))
+    arc_data = arcDF.groupby('zipcode').agg(sum).reset_index()
+
+    zip_data = zipshp.merge(arc_data, left_on='aggZip',right_on='zipcode')
+    zip_data.plot(column=args.time)
+
+    plt.savefig('arcos_'+args.drug+'_'+str(args.year)+'_'+args.time+'_'+'.svg',
+                bbox_inches='tight')
 
 
-# Generate shapefiles with specific drug/year data
-code = '1100'
-amph06DF = arcDF[(arcDF.drugName==code) & (arcDF.rptStart=='2006-01-01')]
-amphshp = zipshp.merge(amph06DF,left_index=True,right_on='zipcode')
-amphshp = amphshp.merge(zipPop,how='left',left_on='zipcode',right_on='aggZip')
-amphshp.to_file("{}/{}".format(DIR,"amphetamines2006.shp"))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Map ARCOS data.')
+    parser.add_argument('-d', '--data', required=True,
+                        help='Path to the ARCOS datafile')
+    parser.add_argument('-g', '--geo', default='aggzips.shp',
+                        help='Path to the geospatial file for mapping the ARCOS'
+                             ' data. Default: `aggzips.shp` in '
+                             'the working directory.')
+    parser.add_argument('-D', '--drug', 
+                        help='Drug code to select. Use `-D ?` for a list of drugs and their codes.')
+    parser.add_argument('-Y', '--year', type=int,
+                        help='Year to select. Currently 2000 - 2019')
+    parser.add_argument('-T', '--time', choices=['Q1','Q2','Q3','Q4','Total'],
+                        default='Total',
+                        help='Time frame to select. choices=[Q1, Q2, Q3, Q4, Total]')
 
-# Aggregate all the opioid drug data and make a shapefile for it
-opioidDF = arcDF[arcDF.drugCode.str.contains('^9')]
-grouped = opioidDF.groupby(['rptStart','zipcode'])
-opData = grouped.aggregate({'State':'first','Q1':'sum','Q2':'sum','Q3':'sum','Q4':'sum','Total':'sum'})
-opData.reset_index(level=0, inplace=True)
-opData.reset_index(level=0, inplace=True)
 
-op16shp = zipshp.merge(opData[opData.rptStart=='2016-01-01'],how='left',left_index=True,right_on='zipcode')
-op16shp = op16shp.merge(zipPop,how='left',left_on='zipcode',right_on='aggZip')
-op16shp.to_file("{}/{}".format(DIR,"opioids2016.shp"))
+    args = parser.parse_args()
 
+    if args.drug == '?':
+        list_drugs(args)
+    else:
+        run(args)
